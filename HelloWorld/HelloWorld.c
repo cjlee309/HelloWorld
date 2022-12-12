39d5d75d6551cfe50240524928e56d88db822f92
cjlee <cjlee@engistech.com>
2022-12-12 12:59:48 +0900

    chaper 2 excercise

--- HelloWorld/HelloWorld.c
+++ HelloWorld/HelloWorld.c
@@ -2,5 +2,7 @@
 
 void main(int argc, char* argv[])
 {
-	printf("My name is CJ Lee\n");
+	printf("My name is CJ Lee\n"); // #1-1 excercise
+	printf("이창주\n011-9777-141\n"); //#1-2 excercise
+	printf("컴파일, 링크\n"); //#1-3 excercise
 }
